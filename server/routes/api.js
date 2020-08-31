20d5b7046676c0a45b254ebf04a67b19c66a55ef
Dmitriy Surkov <mastdiekin@gmail.com>
2020-08-31 22:35:00 +0300

    move

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const defaultApi = require("../controllers/pages");
 const steamApi = require("../controllers/api/steam");
-const error = require("../controllers/api/error");
+const error = require("../controllers/pages/error");
 
 router.use("/", defaultApi);
 router.use("/steam", steamApi);
